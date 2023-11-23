e1163837f8285fb3dd7883c1009319fd76145756
aravindkv27 <aravind27032002@gmail.com>
2023-11-23 10:08:36 +0530

    changelog

--- changelogs/demo1/changelog1.sql
+++ changelogs/demo1/changelog1.sql
@@ -6,7 +6,7 @@ INSERT INTO company (name, address1, address2, city)
 VALUES ('Tech Mach', '123 Tech Lane', 'Tech Park Building', 'Tech City');
 INSERT INTO company (name) VALUES ('Mind Tree.');
 
---changeset Aravind:2 labesl:insert queries context:insert
+--changeset Aravind:2 labesl:insert queries context:insert status=updated
 --comment: Inserting a company with all information
 INSERT INTO company (name, address1, address2, city) 
 VALUES ('L&t', '123 Tech Lane', 'Tech Park Building', 'Tech City');
